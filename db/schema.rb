66daec28a8cbd5b78c6cfa9f56a44be527bcddc5
pimenvibritania <blackbrit13@gmail.com>
2017-12-21 17:16:04 +0700

    upload image and show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219030922) do
+ActiveRecord::Schema.define(version: 20171221083315) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 20171219030922) do
     t.text "summaryeng"
     t.integer "user_id"
     t.integer "category_id"
+    t.string "book_img_file_name"
+    t.string "book_img_content_type"
+    t.integer "book_img_file_size"
+    t.datetime "book_img_updated_at"
   end
 
   create_table "categories", force: :cascade do |t|
