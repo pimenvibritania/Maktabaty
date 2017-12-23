7bfa26937c49d4e8cc2fb7f100ecf57295733f7c
pimenvibritania <blackbrit13@gmail.com>
2017-12-23 11:53:08 +0700

    add carousel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221102849) do
+ActiveRecord::Schema.define(version: 20171223030941) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 20171221102849) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_comments_on_book_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
